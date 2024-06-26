e2f51115066503e7d6a4c4288498633a731a66d0
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-06-26 18:59:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+class App extends React.Component{
+
+
+  
+}
+export default App;
+
+// functional Components 
+
+// <Learning/>  Class components 
+
+/*<Learn name = 'Mango' weight="120gm"/>
+<Learn name = 'Stawberry' weight="120gm"/>  // props
+<Learn name = 'Apple' weight="50gm"/> */
+
+// <img src={logo} classname ="App-logo" alt="logo"/>  JSX 
